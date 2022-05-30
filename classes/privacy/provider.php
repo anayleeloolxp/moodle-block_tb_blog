1adf5e0ea40de1be114f94472885b94932499315
anayleeloolxp <anayjain@leeloolxp.com>
2022-05-30 15:54:43 +0530

    done code formatting

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -25,8 +25,6 @@
 
 namespace block_tb_blog\privacy;
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Privacy Subsystem for block_tb_blog implementing null_provider.
  *
--- settings.php
+++ settings.php
@@ -33,7 +33,7 @@ if ($ADMIN->fulltree) {
         get_string('license', 'block_tb_blog'),
         0
     );
-    //$setting->set_updatedcallback(block_tb_blog_updateconf());
+
     $settings->add($setting);
 
     $setting = new admin_setting_configblog('block_tb_blog/settingsjson', '', '', '', PARAM_RAW);
